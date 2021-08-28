7460c285f5daf2485bd1e3c794aca773fa64ede1
JdovalF <j.doval.fraga@gmail.com>
2021-08-28 12:15:37 +0200

    Se finaliza backend para chat-app con websockets

---
+++ src/test/java/com/jadovalf/wschatback/WsChatBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jadovalf.wschatback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsChatBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
